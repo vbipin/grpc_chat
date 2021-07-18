c0cc1eaa77a30b4d6758c78297a17d071119147f
client-vminit-readonly <client-vminit-readonly@kipitapp.cn>
2021-07-18 21:46:44 +0800

    server additions

--- chat_client.py
+++ chat_client.py
@@ -5,13 +5,23 @@ import grpc
 import chat_pb2
 import chat_pb2_grpc
 
+from chat_pb2 import Status, User, Packet
 
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = chat_pb2_grpc.ChatStub(channel)
-        response = stub.add_user(chat_pb2.User(name='you'))
-    print("Greeter client received: ", response.packets)
-
+        response = stub.add_user(User(name='you'))
+        print("received status: ", response.packets)
+        
+        response = stub.send_msg(Packet(name='you'))
+        print("received status: ", response.packets)
+        
+        response = stub.get_msg(User(name='you'))
+        print("received msg: ", response)
+        
+        response = stub.del_user(User(name='you'))
+        print("received status: ", response.packets)
+        
 
 if __name__ == '__main__':
     run()
--- chat_server.py
+++ chat_server.py
@@ -11,35 +11,43 @@ from chat_pb2 import Status, User, Packet
 
 
 class Chat(chat_pb2_grpc.ChatServicer):
-
-    def SayHello(self, request, context):
-        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
     
     def add_user(self, request, context):
         """Adds a new user to the server
         """
-        return Status(packets=92)
+        username =  request.name
+        store_add_user( username )
+        return Status(packets=num_pending_messages(username))
 
 
     def del_user(self, request, context):
         """Deletes a user
         """
+        username =  request.name
+        store_del_user( username )
         return Status(packets=0)
 
     def get_status(self, request, context):
         """Get status
         """
-        return Status(packets=0)
+        username =  request.name
+        return Status(packets=num_pending_messages(username))
 
     def send_msg(self, request, context):
         """Sends a message to the server
         """
-        return Status(packets=0)
+        username =  request.name
+        msg = request
+        store_put_msg( msg )
+        return Status(packets=num_pending_messages(username))
 
     def get_msg(self, request, context):
         """Read one msg from the server
         """
-        return Packet(name='1')
+        username =  request.name
+        msg = store_get_msg( username )
+        #return Packet(name='1')
+        return msg #this is of type Packet
 
 
 def serve():
@@ -48,6 +56,65 @@ def serve():
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
+    
+    
+####### APP specific code
+##############################################################################
+#We need a message store to keep all the messages
+#We also need a user store to keep track of the logged in users. 
+#It will also store the index of the next expected message
+
+MAX_MSGS = 1024
+msg_store = []
+users = { }  #uid : next msg ptr
+
+def store_add_user(username) :
+    users[username] = 0 #last msg index read
+    
+def store_del_user(username):
+    if username in users:
+        del users[username]
+        store_cleanup()
+        
+def num_pending_messages(username):
+    if len(msg_store) == 0 :
+        return 0
+    #else we count from current index to end of msg_store
+    return len(msg_store) - users[username]  #this will give the pending messages
+           
+def store_put_msg( msg ):
+    msg_store.append(msg)
+    
+    
+def store_cleanup():
+    #we find the min user pointer and purge up to that
+    if not users : #we can purge all
+        #msg_store = []
+        return 
+    
+    min_index = min(users.values())
+    if min_index == 0 : #nothing to do
+        return
+    
+    #adjust the indexs
+    for _ in range(min_index):
+        msg_store.pop(0) #just remove from the back
+        
+    for username in users :
+        users[username] -= min_index
+        
+        
+def store_get_msg( username ) :
+    """returns none if cannot get msg"""
+    store_cleanup()
+    index_to_read = users[username]
+    if index_to_read >= len(msg_store) :
+        return None
+    msg = msg_store[ index_to_read ]
+    users[username] += 1
+    return msg
+
+##############################################################################
 
 
 if __name__ == '__main__':
